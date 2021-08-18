a70ee431bbfb92f612dd89538d64b42b32c0e196
Ignacio Seco <ignacio.seco@inacapmail.cl>
2021-08-18 19:35:22 -0400

    Cambios del /model/server.js unificados

--- models/server.js
+++ models/server.js
@@ -11,12 +11,9 @@ class Server {
         this.port=process.env.PORT;
         this.server=http.createServer(this.app);
         this.auth = '/api/auth';
-<<<<<<< HEAD
         this.curso = '/api/curso';
         this.asignatura = '/api/asignatura';
-=======
         this.usuarios = '/api/usuarios';
->>>>>>> master
         dbConection();
     }
 
@@ -25,12 +22,9 @@ class Server {
         this.app.use(express.json());
         this.app.use(cors());
         this.app.use(this.auth,require('../routes/auth'));
-<<<<<<< HEAD
         this.app.use(this.curso,require('../routes/curso'));
         this.app.use(this.asignatura,require('../routes/asignatura'));
-=======
         this.app.use(this.usuarios,require('../routes/usuarios'));
->>>>>>> master
        
     }
 
