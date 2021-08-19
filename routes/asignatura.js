d1b673256e7d2dfb161819b7ff14bd48eb191e66
Ignacio Seco <ignacio.seco@inacapmail.cl>
2021-08-19 12:54:43 -0400

    Validaciones con express-validator en curso y asignatura

--- routes/asignatura.js
+++ routes/asignatura.js
@@ -1,14 +1,29 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const validarCampos = require('../middlewares/validarcampos');
+
 const { getAsignatura, crearAsignatura, actulizarAsignatura, eliminarAsignatura } = require('../controllers/asignatura');
 
 const router = Router();
 
 router.get('/', getAsignatura );
 
-router.post('/', crearAsignatura);
+router.post('/', [
+    check('idCurso', 'El id del curso no es válido').isMongoId(),
+    check('asignatura','La asignatura es requerida').not().isEmpty(),
+    validarCampos
+], crearAsignatura);
 
-router.put('/:id', actulizarAsignatura);
+router.put('/:id', [
+    check('idCurso', 'El id del curso no es válido').isMongoId(),
+    check('asignatura','La asignatura es requerida').not().isEmpty(),
+    check('id','El id no es válido').isMongoId(),
+    validarCampos
+], actulizarAsignatura);
 
-router.delete('/:id', eliminarAsignatura);
+router.delete('/:id', [
+    check('id','El id no es válido').isMongoId(),
+    validarCampos
+], eliminarAsignatura);
 
 module.exports = router;
--- routes/curso.js
+++ routes/curso.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+const validarCampos = require('../middlewares/validarcampos');
 
 const { getCursos, crearCurso, actualizarCurso, eliminarCurso } = require('../controllers/curso');
 
@@ -7,10 +9,22 @@ const router = Router();
 
 router.get('/', getCursos);
 
-router.post('/', crearCurso);
+router.post('/',[
+    check('letra','La letra del curso es requerida').not().isEmpty(),
+    check('curso','El nombre del curso es requerido').not().isEmpty(),
+    validarCampos
+], crearCurso);
 
-router.put('/:id', actualizarCurso);
+router.put('/:id', [
+    check('letra','La letra del curso es requerida').not().isEmpty(),
+    check('curso','El nombre del curso es requerido').not().isEmpty(),
+    check('id','El id no es válido').isMongoId(),
+    validarCampos
+], actualizarCurso);
 
-router.delete('/:id', eliminarCurso);
+router.delete('/:id',[
+    check('id','El id no es valido').isMongoId(),
+    validarCampos
+], eliminarCurso);
 
 module.exports = router;
